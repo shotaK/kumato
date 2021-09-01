d079fdf50d82ee5d2bac780d5e295737c6f50321
Shota <sh.karkashadze@gmail.com>
2021-09-01 13:17:40 +0400

    Update content.js

--- public/content.js
+++ public/content.js
@@ -9,7 +9,6 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
   chrome.storage.local.get(null, ({ cycleRunning, blockableWebsites }) => {
     if (cycleRunning) {
       blockableWebsites.forEach((blockableWebsite) => {
-        console.log(blockableWebsite, isUrlBlocked(blockableWebsite));
         if (isUrlBlocked(blockableWebsite)) {
           window.location = chrome.runtime.getURL("/pageBlocked.html");
         }
