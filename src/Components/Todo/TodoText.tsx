541df35c473999d8535861b79a5c47c3b3509d69
Shota <sh.karkashadze@gmail.com>
2022-06-12 18:12:07 +0400

    Fix word breaking with todo text

--- src/Components/Todo/TodoText.tsx
+++ src/Components/Todo/TodoText.tsx
@@ -17,7 +17,7 @@ const TodoText = memo(({ todo }: { todo: Todo }) => {
   return (
     <span
       className={classNames(
-        "text-gray-200 text-sm flex-1",
+        "text-gray-200 text-sm flex-1 break-all",
         {
           "line-through text-gray-400": isComplete,
         },
